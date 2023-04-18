b80e3b5fda84f9b1c7c2d122a7a2cb732052b0a4
Ahad1713 <khanazan761@gmail.com>
2023-04-18 19:34:17 +0530

    :art: fetching records

--- src/components/SmoothCard.js
+++ src/components/SmoothCard.js
@@ -1,9 +1,16 @@
+import { Link } from "react-router-dom"
+
 const SmoothCard = ({ smooth }) => {
     return (
       <div className="smooth-card">
         <h3>{smooth.title}</h3>
         <p>{smooth.method}</p>
         <div className="rating">{smooth.rating}</div>
+        <div className="buttons">
+        <Link to={"/" + smooth.id}>
+          <i className="material-icons">edit</i>
+        </Link>
+      </div>
       </div>
     )
   }

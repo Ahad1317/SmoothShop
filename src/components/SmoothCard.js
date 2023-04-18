0d580c94d6bc34046ff156789df479cc1a86af45
Ahad1713 <khanazan761@gmail.com>
2023-04-18 19:48:03 +0530

    :sparkles: delete records

--- src/components/SmoothCard.js
+++ src/components/SmoothCard.js
@@ -1,6 +1,22 @@
 import { Link } from "react-router-dom"
+import supabase from "../config/supabaseClient"
 
 const SmoothCard = ({ smooth }) => {
+
+  const handleDelete = async () => {
+    const { data, error } = await supabase
+      .from('smoothes')
+      .delete()
+      .eq('id', smooth.id)
+    
+    if (error) {
+      console.log(error)
+    }
+    if (data) {
+      console.log(data)
+    }
+  }
+
     return (
       <div className="smooth-card">
         <h3>{smooth.title}</h3>
@@ -10,6 +26,7 @@ const SmoothCard = ({ smooth }) => {
         <Link to={"/" + smooth.id}>
           <i className="material-icons">edit</i>
         </Link>
+        <i className="material-icons" onClick={handleDelete}>delete</i>
       </div>
       </div>
     )
